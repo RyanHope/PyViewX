26a93d4865b6d737ea96f610961cc0b568b8cf04
Ryan Hope <rmh3093@gmail.com>
2012-02-19 10:11:30 -0500

    update documentation

--- PyViewX/client.py
+++ PyViewX/client.py
@@ -1,10 +1,4 @@
-"""
-.. module:: client
-	:platform: Unix
-
-.. moduleauthor:: Ryan Hope <rmh3093@gmail.com>
-"""
-
+# -*- coding:	utf-8 -*-
 #===============================================================================
 # This file is part of PyViewX.
 # Copyright (C) 2012 Ryan Hope <rmh3093@gmail.com>
@@ -22,6 +16,13 @@
 # You should have received a copy of the GNU General Public License
 # along with PyViewX.  If not, see <http://www.gnu.org/licenses/>.
 #===============================================================================
+
+"""
+.. module:: client
+	:platform: Unix
+
+.. moduleauthor:: Ryan Hope <rmh3093@gmail.com>
+"""
 
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor, defer
@@ -57,14 +58,12 @@ class iViewXClient( DatagramProtocol ):
 			print ['UNHANDLED', data]
 
 	def __sendCommand( self, *args, **kwargs ):
-		if not self.deferreds.has_key( args[0] ):
-			self.deferreds[args[0]] = deque()
-		d = defer.Deferred()
 		if kwargs['callback']:
+			if not self.deferreds.has_key( args[0] ):
+				self.deferreds[args[0]] = deque()
+			d = defer.Deferred()
 			d.addCallback( kwargs['callback'] )
-		else:
-			d.addCallback( self.__noop )
-		self.deferreds[args[0]].appendleft( d )
+			self.deferreds[args[0]].appendleft( d )
 		self.transport.write( '%s\n' % ' '.join( map( str, args ) ) )
 
 	#===========================================================================
@@ -72,10 +71,12 @@ class iViewXClient( DatagramProtocol ):
 	#===========================================================================
 
 	def startCalibration( self, points, eye = 0, callback = None ):
-		"""Start calibration procedure.
+		"""Starts a calibration. Returns calibration information is successful.
 
 		:param points: The number of calibration points; valid options are 2, 5, 9 or 13.
 		:type points: int.
+		:param eye: The eye to use for binocular systems; valid options are 1-right or 2-left.
+		:type eye: int.
 		:param callback: A function to call with response.
 		:type callback: function.
 
@@ -90,16 +91,21 @@ class iViewXClient( DatagramProtocol ):
 			self.__sendCommand( 'ET_CAL', points, callback = callback )
 
 	def acceptCalibrationPoint( self, callback = None ):
-		"""Accept calibration point.
+		"""Accepts the current calibration point during the calibration process,
+		and switches to the next calibration point. Returns the number of the next
+		calibration point if successful. Available only during calibration.
 
 		:param callback: A function to call with response.
 		:type callback: function.
+
+		*The command is sent by iViewX every time a calibration point is accepted
+		during calibration, either manually by the user or automatically.*
 
 		"""
 		self.__sendCommand( 'ET_ACC', callback = callback )
 
 	def cancelCalibration( self, callback = None ):
-		"""Cancel calibration procedure.
+		"""Cancels the calibration procedure.
 
 		:param callback: A function to call with response.
 		:type callback: function.
@@ -121,24 +127,33 @@ class iViewXClient( DatagramProtocol ):
 		self.__sendCommand( 'ET_CPA', param, callback = callback )
 
 	def setCalibrationParam( self, param, value, callback = None ):
-		"""Set calibration parameter.
+		"""Sets calibration parameters.
 
-		:param param: Numeric ID of calibration parameter; valid options are 0, 1, 2 and 3.
+		===== ====================== ======= =======
+		param description            value=0 value=1
+		===== ====================== ======= =======
+		0     wait for valid data    off     on
+		1     randomize point order  off     on
+		2     auto accept            off     on
+		3     calibration speed      slow    fast
+		===== ====================== ======= =======
+
+		:param param: Numeric ID of calibration parameter.
 		:type param: int.
-		:param value: New state of calibration parameter; valid options are True=On, False=Off.
-		:type value: bool.
+		:param value: New state of calibration parameter.
+		:type value: int.
 		:param callback: A function to call with response.
 		:type callback: function.
 
 		"""
 		if ( param < 0 or param > 3 ):
 			raise PyViewXception( 'ET_CPA', 'Invalid param' )
-		if not isinstance( value, bool ):
+		if not isinstance( value, int ):
 			raise PyViewXception( 'ET_CPA', 'Value not boolean' )
-		self.__sendCommand( 'ET_CPA', param, int( value ), callback = callback )
+		self.__sendCommand( 'ET_CPA', param, value, callback = callback )
 
 	def setSizeCalibrationArea( self, width, height, callback = None ):
-		"""Set the size of the calibration area.
+		"""Sets the size of the calibration area.
 
 		:param width: Width of calibration area in pixels.
 		:type width: int.
@@ -147,13 +162,15 @@ class iViewXClient( DatagramProtocol ):
 		:param callback: A function to call with response.
 		:type callback: function.
 
+		*The command is sent by iViewX when the size of the calibration area is changed.*
+
 		"""
 		if not ( isinstance( width, int ) and isinstance( height, int ) and width > 0 and height > 0 ):
 			raise PyViewXception( 'ET_CSZ', 'Invalid dimension' )
 		self.__sendCommand( 'ET_CSZ', width, height, callback = callback )
 
 	def resetCalibrationPoints( self, callback = None ):
-		"""Reset calibration points to default values.
+		"""Sets all calibration points to default positions.
 
 		:param callback: A function to call with response.
 		:type callback: function.
@@ -162,9 +179,9 @@ class iViewXClient( DatagramProtocol ):
 		self.__sendCommand( 'ET_DEF', callback = callback )
 
 	def setCalibrationCheckLevel( self, value, callback = None ):
-		"""Set calibration check level.
+		"""Sets check level for calibration. Returns the new check level is successful.
 
-		:param value: Calibration check level; valid values are 0, 1, 2 or 3.
+		:param value: Calibration check level; valid values are 0=none, 1=weak, 2=medium or 3=strong.
 		:type value: int.
 		:param callback: A function to call with response.
 		:type callback: function.
@@ -175,7 +192,7 @@ class iViewXClient( DatagramProtocol ):
 		self.__sendCommand( 'ET_LEV', value, callback = callback )
 
 	def setCalibrationPoint( self, point, x, y, callback = None ):
-		"""Set location of calibration point.
+		"""Sets the position of a given calibration point.
 
 		:param point: Calibration point.
 		:type point: int.
@@ -188,14 +205,16 @@ class iViewXClient( DatagramProtocol ):
 
 		.. note::
 			Not available on RED systems.
-
 		"""
 		if not ( isinstance( point, int ) and point > 0 and point < 14 and isinstance( x, int ) and isinstance( y, int ) and x > 0 and y > 0 ):
 			raise PyViewXception( 'ET_PNT', 'Invalid point' )
 		self.__sendCommand( 'ET_PNT', point, x, y, callback = callback )
 
 	def startDriftCorrection( self, callback = None ):
-		"""Start drift correction.
+		"""Starts drift correction. Drift correction is available after a
+		calibration of the system. Drift correction uses the first calibration
+		point, which is usually the center point, as calibration point.
+
 
 		:param callback: A function to call with response.
 		:type callback: function.
@@ -204,10 +223,14 @@ class iViewXClient( DatagramProtocol ):
 			Only for hi-speed systems.
 
 		"""
-		self.__sendCommand( 'ET_DEF', callback = callback )
+		self.__sendCommand( 'ET_RCL', callback = callback )
 
 	def validateCalibrationAccuracy( self, callback = None ):
-		"""Validate calibration accuracy.
+		"""Performs a validation of the calibration accuracy. This command is
+		available only if a successful calibration has been performed previously.
+		The result shows the accuracy of the calibration and therefore indicates
+		its quality. With the return values you can estimage before starting the
+		experiment, how good the measurement will be.
 
 		:param callback: A function to call with response.
 		:type callback: function.
@@ -216,7 +239,11 @@ class iViewXClient( DatagramProtocol ):
 		self.__sendCommand( 'ET_VLS', callback = callback )
 
 	def validateCalibrationAccuracyExtended( self, x, y, callback = None ):
-		"""Validate calibration accuracy (extended).
+		"""Performs an extended calibration validation of a single point. This
+		command is available only if a successful calibration has been performed
+		previously. THe result shows the accuracy of the calibration and therefore
+		indicates its quality. With the return values you can estimate before
+		starting the experiment, how good the measurement will be.
 
 		:param x: Horizontal position of calibration test point in pixels.
 		:type x: int.
@@ -231,7 +258,8 @@ class iViewXClient( DatagramProtocol ):
 		self.__sendCommand( 'ET_VLX', x, y, callback = callback )
 
 	def requestCalibrationResults( self, callback = None ):
-		"""Request calibration resuts.
+		"""Requests iViewX for calibration results and returns the gaze data
+		aquired for a specific calibration point.
 
 		:param callback: A function to call with response.
 		:type callback: function.
@@ -244,48 +272,65 @@ class iViewXClient( DatagramProtocol ):
 	#===========================================================================
 
 	def setDataFormat( self, frm, callback = None ):
-		"""Set the format of streaming data.
+		"""Sets data format for data output. The syntax is similar to the 'C'
+		string formatting syntax. Each format specifier is marked by a preceding
+		percentage (%) symbol.
+
+		================ ===================================================
+		Format specifier Description
+		================ ===================================================
+		TS               timestamp in milliseconds (0 ...2^64/1000 ms)
+		TU               timestamp in microseconds (0 ...2^64 μs)
+		DX,DY            pupil diameter (0 ...2^32 pixels) x 32
+		PX,PY            pupil position (± 2^31 pixels) x 32
+		CX,CY            corneal reflex position (± 2^31 pixels) x 32
+		SX,SY            gaze position (± 2^31 pixels)
+		SC               scene counter
+		ET               eye type information (l-left, r-right, b-binocular)
+		================ ===================================================
+
+		Example for monocular data:
+			**%TS: %SX, %SY**
+		Result:
+			**28437864110: 400, 202**
+
+		Example for binocular data:
+			**%ET %SX %SY**
+		Result:
+			**b 399 398 200 199**
 
 		:param frm: The format of the streamed data.
 		:type frm: str.
-		:param callback: A function to call with response.
-		:type callback: function.
-
 		"""
 		if not isinstance( frm, str ):
 			raise PyViewXception( 'ET_FRM', 'Not a string' )
-		self.__sendCommand( 'ET_FRM', '"%s"' % frm, callback = callback )
+		self.__sendCommand( 'ET_FRM', '"%s"' % frm )
 
-	def startDataStreaming( self, framerate = 0, callback = None ):
-		"""Start data streaming.
+	def startDataStreaming( self, framerate = 0 ):
+		"""Starts continuous data output (streaming) using the output format
+		specified with the :func:`setDataFormat` command. Optionally, the
+		frame rate can be set at which the data will be streamed.
 
 		:param framerate: Set framerate -- 1..SampleRate. [*optional*]
 		:type framerate: int.
-		:param callback: A function to call with response.
-		:type callback: function.
-
 		"""
-		self.__sendCommand( 'ET_EST', callback = callback )
+
 		if isinstance( framerate, int ) and framerate > 0:
-			self.__sendCommand( 'ET_STR', framerate = framerate, callback = callback )
+			self.__sendCommand( 'ET_STR', framerate = framerate )
 		else:
-			self.__sendCommand( 'ET_STR', callback = callback )
+			self.__sendCommand( 'ET_STR' )
 
-	def stopDataStreaming( self, callback = None ):
-		"""Stop data streaming.
+	def stopDataStreaming( self ):
+		"""Stops continuous data output (streaming)."""
 
-		:param callback: A function to call with response.
-		:type callback: function.
-
-		"""
-		self.__sendCommand( 'ET_EST', callback = callback )
+		self.__sendCommand( 'ET_EST' )
 
 	#===========================================================================
 	# Other
 	#===========================================================================
 
 	def getSampleRate( self, callback = None ):
-		"""Get sample rate.
+		"""Returns current sample rate.
 
 		:param callback: A function to call with response.
 		:type callback: function.
