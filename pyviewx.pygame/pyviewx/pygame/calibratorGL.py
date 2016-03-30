3cfe43c247d4a9bee741ca0bb05445c9d7c4ffa9
cogworks <cogworks@exp2-cogworks.cogsci.rpi.edu>
2016-03-30 13:29:43 -0400

    fix whitespace

--- pyviewx.pygame/pyviewx/pygame/calibratorGL.py
+++ pyviewx.pygame/pyviewx/pygame/calibratorGL.py
@@ -1,4 +1,4 @@
-# -*- coding:	utf-8 -*-
+# -*- coding:    utf-8 -*-
 #===============================================================================
 # This file is part of PyViewX.
 # Copyright (C) 2012-2013 Ryan Hope <rmh3093@gmail.com>
@@ -26,51 +26,51 @@ def mean(l): return sum(l) / len(l)
 
 class CalibratorGL(Calibrator):
 
-	def _init_screen(self):
-		if screen:
-			self.screen = screen
-		else:
-			pygame.display.init()
-			pygame.font.init()
-			self.screen = pygl2d.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF)
+    def _init_screen(self):
+        if screen:
+            self.screen = screen
+        else:
+            pygame.display.init()
+            pygame.font.init()
+            self.screen = pygl2d.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.DOUBLEBUF)
 
-	def _draw_text(self, text, font, color, loc):
-		t = pygl2d.font.RenderText(text, color, font)
-		tr = t.get_rect()
-		tr.center = loc
-		t.draw(tr.topleft)
+    def _draw_text(self, text, font, color, loc):
+        t = pygl2d.font.RenderText(text, color, font)
+        tr = t.get_rect()
+        tr.center = loc
+        t.draw(tr.topleft)
 
-	def _display(self):
+    def _display(self):
         pygl2d.display.begin_draw(self.screen_size)
         pygl2d.draw.rect(self.worldsurf_rect, (51, 51, 153))
-		if self.state == 0:
-			r = pygame.Rect(0, 0, 0, 0)
-			r.width = int((40 * self.width) / 100)
-			r.height = int((20 * self.height) / 100)
-			r.center = (self.center_x, self.center_y)
-			pygl2d.draw.rect(r, (0, 0, 0), 1)
-			f = pygame.font.Font(None, 18)
-			if self.eye_position and self.eye_position[4] > 550 and self.eye_position[5] > 550 and self.eye_position[4] < 850 and self.eye_position[5] < 850:
-				left = map(int, (self.eye_position[0] / 99.999 * self.center_x + self.center_x, self.eye_position[2] / -99.999 * self.center_y + self.center_y))
-				right = map(int, (self.eye_position[1] / 99.999 * self.center_x + self.center_x, self.eye_position[3] / -99.999 * self.center_y + self.center_y))
-				l = int((700 - self.eye_position[4]) / 7 + 20)
-				r = int((700 - self.eye_position[5]) / 7 + 20)
-				if l > 0:
-					pygl2d.draw.circle(left, l, (255, 255, 255))
-				if r > 0:
-					pygl2d.draw.circle(right, r, (255, 255, 255))
-				self._draw_text('%d' % int(self.eye_position[4] - 700), f, (0, 0, 0), left)
-				self._draw_text('%d' % int(self.eye_position[5] - 700), f, (0, 0, 0), right)
-			if not self.currentPoint < 0:
-				pygl2d.draw.circle(self.calibrationPoints[self.currentPoint], 8, (255, 255, 0))
-				pygl2d.draw.circle(self.calibrationPoints[self.currentPoint], 2, (0, 0, 0))
-		if self.state > 0:
-			f = pygame.font.Font(None, 28)
-			if not self.calibrationResults:
-				self._draw_text('Calculating calibration accuracy %s' % self.spinner[self.spinnerIndex], f, (255, 255, 255), (self.center_x, self.center_y))
-			else:
-				self._draw_text(' '.join(self.calibrationResults[0]), f, (255, 255, 255), (self.center_x, self.center_y))
-				if len(self.calibrationResults) > 1:
-					self._draw_text(' '.join(self.calibrationResults[1]), f, (255, 255, 255), (self.center_x, self.center_y + 30))
-				self._draw_text("Press 'R' to recalibrate, press 'Space Bar' to continue...", f, (255, 255, 255), (self.center_x, self.height - 60))
+        if self.state == 0:
+            r = pygame.Rect(0, 0, 0, 0)
+            r.width = int((40 * self.width) / 100)
+            r.height = int((20 * self.height) / 100)
+            r.center = (self.center_x, self.center_y)
+            pygl2d.draw.rect(r, (0, 0, 0), 1)
+            f = pygame.font.Font(None, 18)
+            if self.eye_position and self.eye_position[4] > 550 and self.eye_position[5] > 550 and self.eye_position[4] < 850 and self.eye_position[5] < 850:
+                left = map(int, (self.eye_position[0] / 99.999 * self.center_x + self.center_x, self.eye_position[2] / -99.999 * self.center_y + self.center_y))
+                right = map(int, (self.eye_position[1] / 99.999 * self.center_x + self.center_x, self.eye_position[3] / -99.999 * self.center_y + self.center_y))
+                l = int((700 - self.eye_position[4]) / 7 + 20)
+                r = int((700 - self.eye_position[5]) / 7 + 20)
+                if l > 0:
+                    pygl2d.draw.circle(left, l, (255, 255, 255))
+                if r > 0:
+                    pygl2d.draw.circle(right, r, (255, 255, 255))
+                self._draw_text('%d' % int(self.eye_position[4] - 700), f, (0, 0, 0), left)
+                self._draw_text('%d' % int(self.eye_position[5] - 700), f, (0, 0, 0), right)
+            if not self.currentPoint < 0:
+                pygl2d.draw.circle(self.calibrationPoints[self.currentPoint], 8, (255, 255, 0))
+                pygl2d.draw.circle(self.calibrationPoints[self.currentPoint], 2, (0, 0, 0))
+        if self.state > 0:
+            f = pygame.font.Font(None, 28)
+            if not self.calibrationResults:
+                self._draw_text('Calculating calibration accuracy %s' % self.spinner[self.spinnerIndex], f, (255, 255, 255), (self.center_x, self.center_y))
+            else:
+                self._draw_text(' '.join(self.calibrationResults[0]), f, (255, 255, 255), (self.center_x, self.center_y))
+                if len(self.calibrationResults) > 1:
+                    self._draw_text(' '.join(self.calibrationResults[1]), f, (255, 255, 255), (self.center_x, self.center_y + 30))
+                self._draw_text("Press 'R' to recalibrate, press 'Space Bar' to continue...", f, (255, 255, 255), (self.center_x, self.height - 60))
         pygl2d.display.end_draw()
